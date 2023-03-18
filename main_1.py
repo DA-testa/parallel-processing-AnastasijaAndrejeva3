292dae9fa46477ba59f065a4108018cb213989e5
AnastasijaAndrejeva3 <125015510+AnastasijaAndrejeva3@users.noreply.github.com>
2023-03-18 20:26:26 +0000

    22:26

---
+++ main_1.py
@@ -0,0 +1,26 @@
+# Anastasija Andrejeva, 18. grupa, 221RDC028
+import heapq
+
+def parallel_processing(n, m, data):
+    output = []
+    threads = [(0,i) for i in range(n)]
+    heapq.heapify(threads)
+
+    for i, j in enumerate(data):
+        time, job = heapq.heappop(threads)
+        output.append((job, time))
+        heapq.heappush(threads, (time + j, job))
+
+    return output
+
+def main():
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
+
+    result = parallel_processing(n, m, data)
+
+    for i, j in result:
+        print(i,j)
+
+if __name__ == "__main__":
+    main()
